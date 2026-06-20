0716933b51347f9efd75cf6bafedf070e4d586e4
geokting02 <geokting03@gmail.com>
2026-06-21 01:27:31 +0800

    try fail

--- Assets/unity.webp/Runtime/Interop/WebPMuxAnimBlend.cs
+++
@@ -1,9 +0,0 @@
-namespace unity.libwebp.Interop
-{
-    [NativeTypeName("unsigned int")]
-    public enum WebPMuxAnimBlend : uint
-    {
-        WEBP_MUX_BLEND,
-        WEBP_MUX_NO_BLEND,
-    }
-}
--- Assets/unity.webp/Runtime/Interop/WebPMuxAnimDispose.cs
+++
@@ -1,9 +0,0 @@
-namespace unity.libwebp.Interop
-{
-    [NativeTypeName("unsigned int")]
-    public enum WebPMuxAnimDispose : uint
-    {
-        WEBP_MUX_DISPOSE_NONE,
-        WEBP_MUX_DISPOSE_BACKGROUND,
-    }
-}
